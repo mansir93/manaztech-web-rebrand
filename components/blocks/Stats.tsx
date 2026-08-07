3b41d601582768e7b66b8f2c02858d431be21fba
JerryKaz <jerrymyronkaz@gmail.com>
2026-08-07 14:38:02 +0000

    Update delivered projects statistic

--- components/blocks/Stats.tsx
+++ components/blocks/Stats.tsx
@@ -2,7 +2,7 @@
 import { motion } from 'motion/react';
 
 const stats = [
-  { value: '50+', label: 'Projects Delivered' },
+  { value: '15+', label: 'Projects Delivered' },
   { value: '98%', label: 'Client Satisfaction' },
   { value: '24/7', label: 'Support Available' },
   { value: '5+', label: 'Years Experience' },
