04d0d7280d9a250f8d30a1c5a73161d50f72ae3f
Mansir Abdul Aziz <mansiraziz93@gmail.com>
2026-08-07 15:44:29 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div className="">
       <Hero />
-      <Stats />x
+      <Stats />
       <ClientStrip />
       <WhyChooseUs />
       <About />
